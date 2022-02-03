4d01f0d98a152fb6afb63c847562d8a5a1f8c1d2
Lara <61526994+lilahub@users.noreply.github.com>
2022-02-03 15:41:27 -0300

    Add files via upload

---
+++ aplicacaobanco/AplicacaoBanco.java
@@ -0,0 +1,15 @@
+
+package aplicacaobanco;
+
+public class AplicacaoBanco {
+
+
+    public static void main(String[] args) {
+		ClienteDoBanco cliente = new ClienteDoBanco("Madu");
+		ContaBancaria conta = new ContaBancaria("4228-7");
+
+		Fachada facade = new Fachada();
+		facade.fazerDeposito(710, cliente, conta);
+	}
+    
+}
---
+++ aplicacaobanco/ClienteDoBanco.java
@@ -0,0 +1,17 @@
+
+package aplicacaobanco;
+
+public class ClienteDoBanco {
+    private String nome;
+
+	public String obterNomeDoCliente(){
+            return this.nome;
+	}
+
+	public ClienteDoBanco(){}
+
+	public ClienteDoBanco(String nome){
+            this.nome = nome;
+	}
+    
+}
